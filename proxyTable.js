347bacccfb6b9fb1b73999fba58c9967b7fda14b
fhfhfh <fmahon@redhat.com>
2017-12-13 16:09:55 +0000

    test icare route

--- proxyTable.js
+++ proxyTable.js
@@ -2,5 +2,9 @@ module.exports={
   "/wecare-auth":{
     "name":"wecare authentication service",
     "target":"http://auth-fes:8080/wecare-auth"
+  },
+  "/icare-auth":{
+    "name":"icare authentication service",
+    "target":"http://fes-icare-auth-sos-online-dev.apps.sos.eu/"
   }
 }
