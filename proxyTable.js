3b06d084527e04c87e7a66a778a5acf7145d1671
fhfhfh <fmahon@redhat.com>
2017-12-13 23:34:11 +0000

    fsi

--- proxyTable.js
+++ proxyTable.js
@@ -10,5 +10,9 @@ module.exports={
   "/google":{
     "name":"google",
     "target":"http://www.google.ie"
+  },
+  "/cmon":{
+    "name":"google",
+    "target":"https://psdev-mpas6im3edr3vywutlyeky52-evals-dev.mbaas1.tom.redhatmobile.com/application/id"
   }
 }
